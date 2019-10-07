5fc654324c5135c6bfbd6562578e225e662a721e
Mateus Bezerra <mateusbzerra@gmail.com>
2019-10-07 16:56:06 -0300

    Funcionaliade de aceitar e recusar Bookings em conjunto com websocket

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,23 +1,74 @@
-import React, { useState, useEffect } from "react";
-import api from "../../services/api";
-import { Link } from "react-router-dom";
-import "./styles.css";
+import React, { useState, useEffect, useMemo } from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import socketio from 'socket.io-client';
+import './styles.css';
 
 export default function Dashboard() {
+  const [spots, setSpots] = useState([]);
+  const [requests, setRequests] = useState([]);
+
   useEffect(() => {
     async function loadSpots() {
-      const userid = localStorage.getItem("user");
-      const { data } = await api.get("/dashboard", {
-        headers: { userid }
+      const userid = localStorage.getItem('user');
+      const { data } = await api.get('/dashboard', {
+        headers: { userid },
       });
       console.log(data);
       setSpots(data);
     }
     loadSpots();
   }, []);
-  const [spots, setSpots] = useState([]);
+
+  const userid = localStorage.getItem('user');
+  const socket = useMemo(
+    () =>
+      socketio('http://localhost:3333', {
+        query: { userid },
+      }),
+    [userid]
+  );
+  useEffect(() => {
+    socket.on('booking_request', data => {
+      const date = new Date(data.date);
+      const formatedDate = `${date.getDate()}/${date.getMonth()}`;
+      data.date = formatedDate;
+      setRequests([...requests, data]);
+    });
+  }, [requests, socket]);
+  async function handleAccept(id) {
+    await api.post(`/bookings/${id}/approvals`);
+    setRequests(requests.filter(request => request._id !== id));
+  }
+  async function handleReject(id) {
+    await api.post(`/bookings/${id}/rejections`);
+    setRequests(requests.filter(request => request._id !== id));
+  }
   return (
     <>
+      <ul className="notifications">
+        {requests.map(request => (
+          <li key={request._id}>
+            <p>
+              <strong>{request.user.email}</strong> está solicitando uma reserva
+              em <strong>{request.spot.company} </strong> para a data:
+              <strong> {request.date}</strong>
+            </p>
+            <button
+              onClick={() => handleAccept(request._id)}
+              className="accept"
+            >
+              Aceitar
+            </button>
+            <button
+              onClick={() => handleReject(request._id)}
+              className="reject"
+            >
+              Rejeitar
+            </button>
+          </li>
+        ))}
+      </ul>
       <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
@@ -25,7 +76,7 @@ export default function Dashboard() {
               style={{ backgroundImage: `url(${spot.thumbnail_url})` }}
             ></header>
             <strong>{spot.company}</strong>
-            <span> {spot.price ? `R$ ${spot.price}/dia` : "Gratuito"} </span>
+            <span> {spot.price ? `R$ ${spot.price}/dia` : 'Gratuito'} </span>
           </li>
         ))}
       </ul>
